77a84f1c91f49918c690e43d6ed586be48b00ad3
YlcDev <yannicklc.dev@gmail.com>
2016-11-15 16:37:45 +0100

    update FOS ok

--- project/src/ProjectBundle/Entity/User.php
+++ project/src/ProjectBundle/Entity/User.php
@@ -2,111 +2,26 @@
 
 namespace ProjectBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 /**
  * User
  */
-class User
+class User extends BaseUser
 {
     /**
-     * @var int
+     * @var integer
      */
-    private $id;
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var string
-     */
-    private $pass;
-
-    /**
-     * @var string
-     */
-    private $mail;
+    protected $id;
 
 
     /**
      * Get id
      *
-     * @return int
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return User
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set pass
-     *
-     * @param string $pass
-     *
-     * @return User
-     */
-    public function setPass($pass)
-    {
-        $this->pass = $pass;
-
-        return $this;
-    }
-
-    /**
-     * Get pass
-     *
-     * @return string
-     */
-    public function getPass()
-    {
-        return $this->pass;
-    }
-
-    /**
-     * Set mail
-     *
-     * @param string $mail
-     *
-     * @return User
-     */
-    public function setMail($mail)
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    /**
-     * Get mail
-     *
-     * @return string
-     */
-    public function getMail()
-    {
-        return $this->mail;
     }
 }
